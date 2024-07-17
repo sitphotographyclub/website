452d7f2b1300c839e7539c122b8331cf43a14028
Swarnadeep Saha Poddar <code.isspoddar@gmail.com>
2024-07-17 20:09:40 +0530

    Created Initial Team DataBase File

---
+++ src/Databases/TeamDetails.jsx
@@ -0,0 +1,18 @@
+const TeamDetails =
+    [
+        {
+            Fullname: "Arpi Saha"
+            , Image: "aprisaha.jpg"
+            , Position: "Technical Team"
+            , Instagram: "https://www.instagram.com/iamarpisaha"
+            , Linkedin: "https://www.linkedin.com/in/iamarpisaha"
+        }
+    ]
+
+    // {
+    //     Fullname: ""
+    //     , Image: ""
+    //     , Position: "
+    //     , Instagram: ""
+    //     , Linkedin: ""
+    // }
