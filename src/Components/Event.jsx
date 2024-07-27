0e36026a6e8065d03522fbf8f2d93e2d608aa654
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-07-27 11:58:39 +0530

    created card component and added routing to event

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import { EventDetails } from '../Databases/EventDetails'
+import {EventCard} from './Events/EventCard'
 import { NavLink } from 'react-router-dom'
 export default function Event() {
   return (
-    <div> Events
-      <br />
+    <div>
+      <div className="max-w-7xl mx-auto my-8 px-2">
+          <div className="flex justify-center text-2xl md:text-3xl font-bold mb-4">
+              Events
+          </div>
+          <div className='grid gap-8 sm:grid-cols-2 lg:grid-cols-3 p-2 xl:p-5'>
+              {EventDetails.map((event) => (
+                  <EventCard
+                      id={event.id}
+                      title={event.Title}
+                      image={event.Image}
+                      des={event.Description}
+                      location={event.Location}
+                    />
+              )
+              )}
+          </div>
+
+      </div>
+
+      {/* <br />
       <NavLink to='/event/event1'>Event 1</NavLink>
       <br />
-      <NavLink to='/event/event2'>Event 2</NavLink>
+      <NavLink to='/event/event2'>Event 2</NavLink> */}
     </div>
   )
 }
---
+++ src/Databases/EventDetails.jsx
@@ -0,0 +1,18 @@
+export const EventDetails = [
+    {
+        id: 1,
+        Title: "Writey A.I",
+        Image: "https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxrZXlib2FyZHxlbnwwfDB8fHwxNjk5NTI1MDAzfDA&ixlib=rb-4.0.3&q=80&w=1080",
+        Location: "Writey A.I",
+        Description: "Writey A.I is an AI tool that generates original, researched blog posts in minutes, provides content optimization advice, and works with a plagiarism-free rule.",
+        Tags: ["Content Generation", "Marketing", "SEO", "Writing"]
+    },
+    {
+        id: 2,
+        Title: "Writey A.I",
+        Image: "https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxrZXlib2FyZHxlbnwwfDB8fHwxNjk5NTI1MDAzfDA&ixlib=rb-4.0.3&q=80&w=1080",
+        Location: "Writey A.I",
+        Description: "Writey A.I is an AI tool that generates original, researched blog posts in minutes, provides content optimization advice, and works with a plagiarism-free rule.",
+        Tags: ["Content Generation", "Marketing", "SEO", "Writing"]
+    },
+];
