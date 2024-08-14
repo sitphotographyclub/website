149ee0b5dde6a0cb48b6c4997d5526a5c0f92738
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-08-14 09:17:38 +0530

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -21,21 +21,30 @@ const Footer = () => {
                 </ul>
               </div>
               <div className="col-6">
-                <h5 className="text-white mb-3">Support</h5>
+                <h5 className="text-white mb-3">Socials</h5>
                 <ul className="list-unstyled">
                   <li><NavLink className={(e) => e.isActive ? "active" : ""} to="/contact">Contact</NavLink></li>
-                </ul>
+                  <li>
+              <a href="/">Instagram</a>
+            </li>
+            <li>
+              <a href="/">Facebook</a>
+            </li>
+            <li>
+              <a href="/">Linkedin</a>
+            </li>
+          </ul>
               </div>
             </div>
           </div>
           {/* footer left side end  */}
           
           {/* footer right side start  */}
-          <div className="col-12 col-lg-6 ps-lg-5 text-lg-end">
+          <div className="col-12 col-lg-6 ps-lg-5 text-lg-start">
             <h5 className="text-white mb-3">SIT Photography Club</h5>
             <p>Capture stunning moments, showcase your talent, and compete for amazing awards.</p>
             <div className="mt-4">
-              <ul className="list-unstyled d-flex justify-content-center justify-content-lg-end gap-3 fs-4">
+              <ul className="list-unstyled d-flex justify-content-center justify-content-lg-start gap-3 fs-4">
                 <li><a href="https://www.facebook.com/sitphotographyclubofficial/" target="_blank" rel="noopener noreferrer"><i className="bi bi-facebook"></i></a></li>
                 <li><a href="https://www.instagram.com/sitphotoclub/" target="_blank" rel="noopener noreferrer"><i className="bi bi-instagram"></i></a></li>
                 <li><a href="https://www.linkedin.com/company/sit-photography-club/" target="_blank" rel="noopener noreferrer"><i className="bi bi-linkedin"></i></a></li>
