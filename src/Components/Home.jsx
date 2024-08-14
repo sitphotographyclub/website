a5e6fa09e6444caf30fefa955de09a70ca80fe94
Swarnadeep Saha Poddar <code.isspoddar@gmail.com>
2024-08-14 11:42:20 +0530

    Home Updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,10 +5,60 @@ import "./css/Home/fontawesome.css";
 import "./css/Home/owl.css";
 import "./css/Home/home.css";
 import "./design.css"
-
+import TeamDetails from "../Databases/TeamDetails";
+import TeamCard from "./Teams/TeamCard";
 export default function Home() {
+  const TeamGB2 = [{
+    Fullname: "Shreya Prasad"
+    , Image: "shreyaprasad.jpg"
+    , Position: "President"
+    , Instagram: "https://www.instagram.com/_justtt.shreya_"
+    , Linkedin: "https://www.linkedin.com/in/shreya-prasad-0b1397252p"
+  },
+  {
+    Fullname: "Arijit Bhattacharyya"
+    , Image: "arijitbhattacharyya.jpg"
+    , Position: "Secretary"
+    , Instagram: "https://www.instagram.com/darshanxarijit_dz?igsh=M21pcWtqOHZnZTBq"
+    , Linkedin: "https://www.linkedin.com/in/arijit-bhattacharyya-517312282"
+  },
+  {
+    Fullname: "Swarnadeep S P"
+    , Image: "swarnadeeppoddar.jpg"
+    , Position: "Organizer"
+    , Instagram: "https://www.instagram.com/swarnadeepsahapoddar"
+    , Linkedin: "https://www.linkedin.com/in/swarnadeep-saha-poddar"
+  }]
+  const TeamGB = [{
+    Fullname: "Rounak Pramanik"
+    , Image: "rounakpramanik.jpg"
+    , Position: "Content Team"
+    , Instagram: "https://www.instagram.com/rounak.jpeg"
+    , Linkedin: "https://www.linkedin.com/in/rounak-pramanik-90128a227"
+  },
+  {
+    Fullname: "Biswarup Das"
+    , Image: "biswarupdas.jpg"
+    , Position: "Media Team"
+    , Instagram: "https://www.instagram.com/qt_uwuman"
+    , Linkedin: "https://www.linkedin.com/in/biswarup-das-a76606236"
+  },
+  {
+    Fullname: "Urbi Goswami"
+    , Image: "urbigoswami.jpg"
+    , Position: "PR/Marketing Team"
+    , Instagram: ""
+    , Linkedin: "https://www.linkedin.com/in/urbi-goswami-734b36255"
+  },
+  {
+    Fullname: "Agniva Ghosh"
+    , Image: "agnivaghosh.jpg"
+    , Position: "Social Media Executive"
+    , Instagram: ""
+    , Linkedin: ""
+  }]
   return (
-    <div className="w-screen">
+    <div className="w-screen bg-black">
       <div className="w-full bg-black h-[500px] flex flex-col-reverse md:flex-row">
         <div className="w-full md:w1/2  h-full text-white">
           <p className="text-white text-3xl md:text-5xl leading-normal Heading pt-7 pr-7 pl-7 pb-2">Capture the Moment with Our Photography Club</p>
@@ -17,10 +67,10 @@ export default function Home() {
           <p className="text-white Heading pt-2 pr-7 pl-7 pb-2 ">Our photography club is a community of passionate photographers who come together to share their love of the art. We organize regular meetups, workshops, and photo walks to help our members improve their skills and capture stunning images. Whether you're a beginner or a seasoned pro, you'll find a welcoming and supportive environment at our club.</p>
         </div>
         <div className="w-full md:w1/2  h-full">
-        <img  src="./Home/Slider/1.jpg" className="w-full"/>
+          <img src="./Home/Slider/1.jpg" className="w-full" />
         </div>
       </div>
-      <div className="w-full bg-black  text-center">
+      <div className="w-full bg-black text-center">
         <h2 className="Heading text-white text-5xl ">Moment Captured By Us </h2>
         <div className="flex flex-col-reverse md:flex-row gap-10 pr-10 pl-5 pt-5">
           <img className="md:w-1/3 w-full rounded" src="./Home/Slider/1.jpg" />
@@ -33,6 +83,20 @@ export default function Home() {
           <img className="md:w-1/3 w-full rounded" src="./Home/Slider/1.jpg" />
         </div>
       </div>
+      <h1 className="text-white Heading text-center p-4">Our G.B. Member</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-24 pl-4 pr-4 bg-black p-4">
+        {TeamGB2.map((member, indx) => {
+          console.log(member.Fullname);
+          return <TeamCard key={indx} member={member} />;
+        })}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 pl-4 pr-4 bg-black">
+        {TeamGB.map((member, indx) => {
+          console.log(member.Fullname);
+          return <TeamCard key={indx} member={member} />;
+        })}
+      </div>
+
     </div>
   );
 }
--- src/Databases/EventDetails.jsx
+++ src/Databases/EventDetails.jsx
@@ -2,7 +2,7 @@ export const EventDetails = [
     {
         id: 1,
         Title: "Photography Club Orientation 2024",
-        Image: "https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxrZXlib2FyZHxlbnwwfDB8fHwxNjk5NTI1MDAzfDA&ixlib=rb-4.0.3&q=80&w=1080",
+        Image: "./Events/Orientation.jpg",
         Location: "E.E. Seminar Hall , Ground Floor ,SIT Campus",
         Description: "Writey A.I is an AI tool that generates original, researched blog posts in minutes, provides content optimization advice, and works with a plagiarism-free rule.",
         Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
