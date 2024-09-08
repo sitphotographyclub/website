d0dbba5d5225b7b4f7ce6f202119bc9419f3b655
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-09-08 11:36:58 +0530

    Added event page links to components

--- src/Components/ForthPAge.jsx
+++ src/Components/ForthPAge.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./Header"
 import { EventDetails } from "../Databases/EventDetails"
 import { EventImgComp } from "./EventImgComp"
+import { NavLink } from "react-router-dom"
 
 export const ForthPage = () => {
     return <div className="py-60 bg-neutral-800">
@@ -9,7 +10,9 @@ export const ForthPage = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 pt-10 lg:grid-cols-4 lg:gap-6 lg:pt-20 sm:gap-4 md:gap-5 ">
                 {EventDetails.map((event) => (
                     <div key={event.id}>
+                        <NavLink to={`/event/event${event.id}`}>
                         <EventImgComp img={event.Image} text={event.Title} subText={event.Location} />
+                        </NavLink>
                     </div>
                 ))}
             </div>
