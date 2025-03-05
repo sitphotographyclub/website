1115420f1e8c45684733f663b42ca5ef03078ba3
Aniket Ghosh <ghoshaniket1000@gmail.com>
2025-03-05 12:07:07 +0530

    added latest event

--- src/Databases/EventDetails.jsx
+++ src/Databases/EventDetails.jsx
@@ -1,99 +1,140 @@
 import Img1 from "/Events/Orientation_1.jpg";
-import Img2 from "/Events/Prinstine.jpg"
-import Img3 from "/Events/Seminar.jpg"
-import Img4 from "/Events/Interview.jpg"
-import Img5 from "/Events/workshop01.jpg"
-import Img6 from "/Events/rakhsha.jpg"
+import Img2 from "/Events/Prinstine.jpg";
+import Img3 from "/Events/Seminar.jpg";
+import Img4 from "/Events/Interview.jpg";
+import Img5 from "/Events/workshop01.jpg";
+import Img6 from "/Events/rakhsha.jpg";
 import Img7 from "/Events/diwali.jpg";
 import Img8 from "/Events/snaps.jpg";
+import Img9 from "/Events/march-weekly1.jpg";
 
 export const EventDetails = [
-    {
-        id: "Photography-Club-Selection-2024",
-        Title: "Photography Club JEM Selection 2024",
-        Image: Img4,
-        Location: "Google Form",
-        Description: "Calling all creative minds! 📸 Join our Photography Club and be a part of one of our dynamic teams: 🔧 Technical Team: Dive into the world of cameras and equipment.📢 PR Team: Spread the word and promote our events.📝 Content Team: Craft captivating content and design posters.📱 Social Media Team: Take charge of our digital presence.Find your passion, hone your skills, and connect with fellow enthusiasts. Join us today!",
-        Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
-        Date: "Upto 25th September , 2024",
-        Timing: "02:00 PM - 05:00 PM",
-        Link: ""
-    },
-    {
-        id: "Photography-Club-Orientation-2024",
-        Title: "Photography Club Orientation 2024",
-        Image: Img1,
-        Location: "E.E. Seminar Hall , Ground Floor ,SIT Campus",
-        Description: "Elevate your photography game with our team! Join us at our Photography Seminar, Participate in fun activities and interactive sessions that will make this seminar an unforgettable experience. See you soon !",
-        Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
-        Date: "12 August 2024",
-        Timing: "02:00 PM - 05:00 PM",
-        Link:""
-    },
-    {
-        id: "Photography-Club-Seminar-2024",
-        Title: "Photography Club Seminar 2024",
-        Image: Img3,
-        Location: "E.E. Seminar Hall , Ground Floor ,SIT Campus",
-        Description: "Join us for an insightful Photography Seminar hosted by the Sit Photography Club, where seasoned photographers will share techniques, tips, and the art of storytelling through the lens",
-        Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
-        Date: "8 April, 2024",
-        Timing: "10:00 AM - 02:00 PM",
-        Link:""
-    },
-    {
-        id: "Pristine",
-        Title: "Pristine",
-        Image: Img2,
-        Location: "SIT Campus",
-        Description: "Capture the essence of nature's purity and beauty in 'Pristine,' the Sit Photography Club competition for students in classes 9-12. Unveil your perspective on the untouched wonders of our world",
-        Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
-        Date: "13 April 2023",
-        Timing: "10:00 AM - 02:00 PM",
-        Link:""
-    },
-    {
-        id: "Creative-Edge-To-Career-Advantage",
-        Title: "Creative Edge To Career Advantage",
-        Image: Img5,
-        Location: "Microsoft Teams",
-        Description: "Join us for three exciting sessions: 📅Nov 16: Designing for Impact – Dive into visual storytelling and user-centered design to boost your creative skills. 📅Nov 17 & 18: Career Insights from Alumni – Hear from two successful alumni on building a career, with one sharing his journey of balancing a job with his passion for photography.",
-        Tags: ["Designing", "Storytelling", "Event managemant", "Career", "Content Creation", "Photography"],
-        Date: "16 - 18 November 2024",
-        Timing: "05:00 PM - 07:00 PM",
-        Link:""
-    },
-    {
-        id: "Beige and Red Playful Illustrative Raksha Bandhan Instagram Story",
-        Title: "Rakhsha Bandhan photography contest",
-        Image: Img6,
-        Location: "Online",
-        Description: "Celebrate the bond of love and protection with our Rakhsha Bandhan photography contest. Capture the essence of this special day in a single frame and share it with us. The most creative entry will win a special prize!",
-        Tags: ["Storytelling", "Content Creation", "Photography"],
-        Date: "24 August 2024",
-        Timing: "",
-        Link: ""
-    },
-    {
-        id: "diwali",
-        Title: "Diwali & Halloween Photography Contest & Reel contest",
-        Image: Img7,
-        Location: "Online",
-        Description: "Celebrate the festival of lights and the spooky season with our Diwali & Halloween Photography Contest & Reel contest. Capture the essence of these special days in a single frame or a 30-second reel and share it with us. The most creative entry will win a special prize!",
-        Tags: ["Designing", "Storytelling", "Event managemant", "Career", "Content Creation", "Photography"],
-        Date: "5 November 2024",
-        Timing: "",
-        Link: ""
-    },
-    {
-        id: "snaps",
-        Title: "Durga Puja Photography & Reels Contest",
-        Image: Img8,
-        Location: "online",
-        Description: "Capture the essence of Durga Puja in a single frame or a 30-second reel and share it with us. The most creative entry will win a special prize!",
-        Tags: ["Designing", "Storytelling", "Event managemant", "Career", "Content Creation", "Photography"],
-        Date: "14 October 2024",
-        Timing: "",
-        Link: ""
-    }
+  {
+    id: "march-weekly1",
+    Title: "March Magic Weekly Photography Contest",
+    Image: Img9,
+    Location: "Google Form",
+    Description: "Capture the essence of the Spring Cheery Blossoms",
+    Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
+    Date: "4-8 March 2025",
+    Timing: "",
+    Link: "https://forms.gle/4dJT5RDWMwS2djkU6",
+  },
+  {
+    id: "Photography-Club-Selection-2024",
+    Title: "Photography Club JEM Selection 2024",
+    Image: Img4,
+    Location: "Google Form",
+    Description:
+      "Calling all creative minds! 📸 Join our Photography Club and be a part of one of our dynamic teams: 🔧 Technical Team: Dive into the world of cameras and equipment.📢 PR Team: Spread the word and promote our events.📝 Content Team: Craft captivating content and design posters.📱 Social Media Team: Take charge of our digital presence.Find your passion, hone your skills, and connect with fellow enthusiasts. Join us today!",
+    Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
+    Date: "Upto 25th September , 2024",
+    Timing: "02:00 PM - 05:00 PM",
+    Link: "",
+  },
+  {
+    id: "Photography-Club-Orientation-2024",
+    Title: "Photography Club Orientation 2024",
+    Image: Img1,
+    Location: "E.E. Seminar Hall , Ground Floor ,SIT Campus",
+    Description:
+      "Elevate your photography game with our team! Join us at our Photography Seminar, Participate in fun activities and interactive sessions that will make this seminar an unforgettable experience. See you soon !",
+    Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
+    Date: "12 August 2024",
+    Timing: "02:00 PM - 05:00 PM",
+    Link: "",
+  },
+  {
+    id: "Photography-Club-Seminar-2024",
+    Title: "Photography Club Seminar 2024",
+    Image: Img3,
+    Location: "E.E. Seminar Hall , Ground Floor ,SIT Campus",
+    Description:
+      "Join us for an insightful Photography Seminar hosted by the Sit Photography Club, where seasoned photographers will share techniques, tips, and the art of storytelling through the lens",
+    Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
+    Date: "8 April, 2024",
+    Timing: "10:00 AM - 02:00 PM",
+    Link: "",
+  },
+  {
+    id: "Pristine",
+    Title: "Pristine",
+    Image: Img2,
+    Location: "SIT Campus",
+    Description:
+      "Capture the essence of nature's purity and beauty in 'Pristine,' the Sit Photography Club competition for students in classes 9-12. Unveil your perspective on the untouched wonders of our world",
+    Tags: ["Content Generation", "Marketing", "SEO", "Writing"],
+    Date: "13 April 2023",
+    Timing: "10:00 AM - 02:00 PM",
+    Link: "",
+  },
+  {
+    id: "Creative-Edge-To-Career-Advantage",
+    Title: "Creative Edge To Career Advantage",
+    Image: Img5,
+    Location: "Microsoft Teams",
+    Description:
+      "Join us for three exciting sessions: 📅Nov 16: Designing for Impact – Dive into visual storytelling and user-centered design to boost your creative skills. 📅Nov 17 & 18: Career Insights from Alumni – Hear from two successful alumni on building a career, with one sharing his journey of balancing a job with his passion for photography.",
+    Tags: [
+      "Designing",
+      "Storytelling",
+      "Event managemant",
+      "Career",
+      "Content Creation",
+      "Photography",
+    ],
+    Date: "16 - 18 November 2024",
+    Timing: "05:00 PM - 07:00 PM",
+    Link: "",
+  },
+  {
+    id: "Beige and Red Playful Illustrative Raksha Bandhan Instagram Story",
+    Title: "Rakhsha Bandhan photography contest",
+    Image: Img6,
+    Location: "Online",
+    Description:
+      "Celebrate the bond of love and protection with our Rakhsha Bandhan photography contest. Capture the essence of this special day in a single frame and share it with us. The most creative entry will win a special prize!",
+    Tags: ["Storytelling", "Content Creation", "Photography"],
+    Date: "24 August 2024",
+    Timing: "",
+    Link: "",
+  },
+  {
+    id: "diwali",
+    Title: "Diwali & Halloween Photography Contest & Reel contest",
+    Image: Img7,
+    Location: "Online",
+    Description:
+      "Celebrate the festival of lights and the spooky season with our Diwali & Halloween Photography Contest & Reel contest. Capture the essence of these special days in a single frame or a 30-second reel and share it with us. The most creative entry will win a special prize!",
+    Tags: [
+      "Designing",
+      "Storytelling",
+      "Event managemant",
+      "Career",
+      "Content Creation",
+      "Photography",
+    ],
+    Date: "5 November 2024",
+    Timing: "",
+    Link: "",
+  },
+  {
+    id: "snaps",
+    Title: "Durga Puja Photography & Reels Contest",
+    Image: Img8,
+    Location: "online",
+    Description:
+      "Capture the essence of Durga Puja in a single frame or a 30-second reel and share it with us. The most creative entry will win a special prize!",
+    Tags: [
+      "Designing",
+      "Storytelling",
+      "Event managemant",
+      "Career",
+      "Content Creation",
+      "Photography",
+    ],
+    Date: "14 October 2024",
+    Timing: "",
+    Link: "",
+  },
 ];
